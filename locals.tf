f948b14e0140afb544516bbd88e6d0e3ffa45264
Thierry Lei <thierry.lei-ext@fr.clara.net>
2022-04-05 10:18:54 +0200

    AZ-615: Add an option to enable or disable default tags

--- locals.tf
+++ locals.tf
@@ -1,9 +1,4 @@
 locals {
-  default_tags = {
-    env   = var.environment
-    stack = var.stack
-  }
-
   #Allocation method of the firewall's public ip. Note: The Public IP must have a Static allocation and Standard sku. Source: https://www.terraform.io/docs/providers/azurerm/r/firewall.html#public_ip_address_id
   public_ip_allocation_method = "Static"
 
@@ -15,5 +10,4 @@ locals {
   log_analytics_subscription_id     = try(split("/", local.log_analytics_id)[2], null)
   log_analytics_resource_group_name = try(split("/", local.log_analytics_id)[4], null)
   log_analytics_name                = try(split("/", local.log_analytics_id)[8], null)
-
 }
