6dd3468596803236343eb8eade3fe9ca1090e2de
OneGit service account - Renovate <fr-ops4ops+bot-renovate@fr.clara.net>
2025-07-31 12:24:17 +0000

    chore(deps): update terraform claranet/subnet/azurerm to ~> 8.1.0

--- m-network.tf
+++ m-network.tf
@@ -1,6 +1,6 @@
 module "firewall_subnet" {
   source  = "claranet/subnet/azurerm"
-  version = "~> 8.0.0"
+  version = "~> 8.1.0"
 
   environment    = var.environment
   location_short = var.location_short
